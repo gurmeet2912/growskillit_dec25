23ddb2f9dbbe3dcde4eda51feba181e60b989ff2
unknown <gill.gurmeet29@gmail.com>
2025-12-27 15:24:38 +0530

    Assignment3

---
+++ Assignment 3_Java Datatypes.java
@@ -0,0 +1,67 @@
+1.Assignment 1: Personal Details Using Primitive Data Types
+package day1;
+
+public class Assignment_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     int Age = 22;
+	     System.out.println("Age:" +  Age);
+	     float Height = 170.5f;
+	     System.out.println("Height:" +  Height);
+         long Mobile_number = 9876543210L;
+	     System.out.println("Mobile Number:" +  Mobile_number);
+         boolean Is_student = true;
+         System.out.println("Is Student:" + Is_student);
+	}
+
+}
+
+
+2. Assignment 2: Display All Primitive Data Types
+
+package day1;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		byte byteValue = 10;
+        short shortValue = 100;
+        int intValue = 5000;
+        long longValue = 9000000000L;
+        float floatValue = 5.5f;
+        double doubleValue = 99.99;
+        char charValue = 'A';
+        boolean booleanValue = true;
+
+        System.out.println("Byte value: " + byteValue);
+        System.out.println("Short value: " + shortValue);
+        System.out.println("Int value: " + intValue);
+        System.out.println("Long value: " + longValue);
+        System.out.println("Float value: " + floatValue);
+        System.out.println("Double value: " + doubleValue);
+        System.out.println("Char value: " + charValue);
+        System.out.println("Boolean value: " + booleanValue);	
+
+	}
+
+}
+
+3.Assignment 3: Marks of Three Subjects
+
+package day1;
+
+public class Assignment3_marks {
+	public static void main(String[] args) {
+	int marks_1 = 75;
+	int marks_2 = 68;
+	int marks_3 = 82;
+	boolean output = true;
+	System.out.println("Subject 1 Marks:" + marks_1);
+	System.out.println("Subject 2 Marks:" + marks_2);
+	System.out.println("Subject 3 Marks:" + marks_3);
+	System.out.println("Result:" + output);
+}
+}
+
+
